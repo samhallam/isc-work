938bdcfca1ce1be38ed404140aea63d2f3a88be8
user01 <user01@localhost.localdomain>
2017-02-28 16:04:47 +0000

    python directory

---
+++ defining_printing_variables.py
@@ -0,0 +1,8 @@
+#script for defining and variablesprinting variable
+
+course = 'Python'
+rating = 7
+
+print course
+print rating
+
---
+++ python/basics.py
@@ -0,0 +1,26 @@
+#Basics Exercises 
+
+#defining variables
+course = 'Python'
+rating = 7
+print course
+print rating
+
+
+#Pythagoras Exercise
+b=5
+c=7
+a=(b**2+c**2)**0.5
+print a
+
+# data types
+print type (a)
+print type (b)
+print type (c)
+
+#converting between data types
+int ('a')
+print type (a)
+
+
+
