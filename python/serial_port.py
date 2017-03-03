4c23c23ab98c9938354d5d3d6e3c28167e7838f7
user01 <user01@localhost.localdomain>
2017-03-03 11:26:03 +0000

    update Friday morning

---
+++ python/serial_port.py
@@ -0,0 +1,37 @@
+#serial port access session
+
+#!/usr/bin/python2.7
+
+from datetime import datetime
+import serial, io
+
+outfile = '/tmp/serial-temperature.tsv'
+
+ser = serial.Serial(
+    port ='/dev/ttyUSB0',
+    baudrate = 9600,
+    bytesize = serial.EIGHTBITS,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE
+)
+
+
+sio = io.TextIOWrapper(
+    io.BufferedRWPair(ser, ser,1),
+    encoding='ascii', newline = '\r'
+)
+
+#  "8" here is specific to the Papouch thermometer device
+
+with open(outfile,'a') as f: #appends to existing file
+    while ser.isOpen():
+        datastring = sio.readline()
+        #\t is tab; \n is line separator
+        f.write(datetime.utcnow().isoformat() + '\t' + datastring + '\n')
+
+        f.flush() #included to force the stystem to write to disk
+        #print  datetime.utcnow().isoformat(), datastring
+
+ser.close()
+
+# tail- f  '/tmp/serial-temperature.tsv'
